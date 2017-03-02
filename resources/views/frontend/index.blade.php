3b03d61f3a35c46832507611b0359b09567fcb2f
Ikhsan Nugraha <Ikhsan@UNFPA.id>
2017-03-02 09:06:51 +0700

    before merge 2

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -301,7 +301,7 @@
 	<div class="project" id="partner">
 		<div class="container">
             <div class="project-1 clock wow bounceIn">
-                <h3>Indonesia Familly Planning Partners</h3>
+                <h3>Indonesia Family Planning Partners</h3>
                 <br>
                 <center><a href="{{ route('frontend.partner')}}" class="btn btn-default"> Show All Partners</a></center>
             </div>
@@ -414,27 +414,6 @@
 	      </div>
 	      <div class="modal-body">
 	        <p>{{ $objective->body }}</p>
-	      </div>
-	      <div class="modal-footer">
-	        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-	      </div>
-	    </div>
-
-	  </div>
-	</div>
-
-	<!-- Modal -->
-	<div id="myModal5" class="modal fade" role="dialog">
-	  <div class="modal-dialog">
-
-	    <!-- Modal content-->
-	    <div class="modal-content">
-	      <div class="modal-header">
-	        <button type="button" class="close" data-dismiss="modal">&times;</button>
-	        <h4 class="modal-title">All Event</h4>
-	      </div>
-	      <div class="modal-body">
-	        <p>Some text in the modal.</p>
 	      </div>
 	      <div class="modal-footer">
 	        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
